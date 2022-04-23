92ff992cb0faf45e81e09d266a79fd6eb8146ce2
Opata Joshua <opatajoshua@gmail.com>
2022-04-23 22:35:20 +0000

    added additional validations

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 function calculateAge (event) {
   //prevent the form submit from reloading the page
   event.preventDefault();
-  
+
   let year = document.getElementById("year").value;
 
   if (year) {
-      let currentYear = new Date().getFullYear();
-      displayAge(currentYear - year);    
+    let currentYear = new Date().getFullYear();
+    // year is negative or 0
+    if(year<=0){
+      displayError("Please enter a number > 0");
+      return;
+    }
+    // year is greater than current year
+    if(year>currentYear){
+      displayError("Please enter a year less than "+currentYear);
+      return;
+    }
+    // year is greater than current year
+    if(year<1850){
+      displayError("Please enter a year more than "+1850);
+      return;
+    }
+    displayAge(currentYear - year);    
   }
   else {
-      displayError();  
+    displayError("Please enter the year");  
   }  
 }
 
-function displayError() {
+function displayError(errorMessage) {
   let errorEl = document.getElementById("result");
-  errorEl.innerHTML = "Please enter the year";
+  errorEl.innerHTML = errorMessage;
   errorEl.style.color="red";   
 }
 
